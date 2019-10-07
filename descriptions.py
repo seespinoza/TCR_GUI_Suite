f20266c0465ddaf60fa6c0f4fa93a5d21ce9dddc
seespinoza <se.espinoza132@gmail.com>
2019-10-06 21:44:22 -0500

    GUI feature descriptions

---
+++ descriptions.py
@@ -0,0 +1,33 @@
+# Contains string descriptions for GUI features to further explain their
+# functionality.
+
+class Desc:
+
+    # MEME descriptions
+    @staticmethod
+    def classic():
+        return 'Classic Mode', '\tYou provide one set of sequences and MEME discovers motifs enriched in this set. \n'\
+           '\tEnrichment is measured relative to a (higher order) random model based on\n'
+
+    @staticmethod
+    def site_distribution():
+        return ''
+
+    @staticmethod
+    def number_of_motifs():
+        return ''
+
+    # Circos descriptions
+    @staticmethod
+    def label_size():
+        return ''
+
+    @staticmethod
+    def ideogram_thickness():
+        return ''
+
+    @staticmethod
+    def label_position():
+        return ''
+
+
